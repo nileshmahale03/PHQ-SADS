5bbfd5fb6de600b3b95a8364c63d6ff7e7c4aaf0
nileshmahale <nileshmahale03@gmail.com>
2016-01-01 21:23:15 -0800

    user-story-A1: completed, storyboard modified

---
+++ PHQ-SADS/PHQ-SADS/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  PHQ-SADS
+//
+//  Created by Nilesh Mahale on 1/1/16.
+//  Copyright © 2016 Code-Programming. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add title
+        self.navigationItem.title = "Info"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
